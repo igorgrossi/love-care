ac43d0a13bdbe60ac7d7f7e49d69c7776a029174
igorgrossi92@hotmail.com <hugorosello@wizsolucoes.com.br>
2019-11-02 18:14:00 -0300

    implementação da lista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,11 +5,17 @@ import { Component } from "@angular/core";
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
+
+
 export class HomePage {
   carregamentoInicial: boolean;
   openInstituicao: boolean;
   controllerAll: boolean;
   cadastroInstituicao: boolean;
+  abrirListaDoacao: boolean;
+  public listaDoacoes: Array<any>;
+  public listaDoacoesFiltradas: Array<any>;
+
 
   constructor() {
     setTimeout(() => {
@@ -26,11 +32,39 @@ export class HomePage {
     this.openInstituicao = false;
     this.carregamentoInicial = true;
     this.controllerAll = false;
+    this.abrirListaDoacao = false;
     this.cadastroInstituicao = false;
   }
 
   abrirCadastroInstituicao() {
     this.openInstituicao = false;
     this.cadastroInstituicao = true;
+  }
+
+  abrirDoacao() {
+    this.abrirListaDoacao = true;
+    this.controllerAll = true;
+    this.carregarListaDoacoes();
+    this.listaDoacoesFiltradas = this.listaDoacoes;
+  }
+
+  filterDoacoes(e) {
+    if (e === 'todos' || e === '') {
+      this.listaDoacoesFiltradas = this.listaDoacoes;
+    }else{
+      this.listaDoacoesFiltradas = this.listaDoacoes.filter(x => x.categoria.toLowerCase().includes(e.toLowerCase()));
+    }
+    
+  }
+
+
+  carregarListaDoacoes() {
+    this.listaDoacoes = [
+      { nomeItem: 'Feijão', categoria: 'alimento', descricao: 'Precisamos de 30 kg de feijão' },
+      { nomeItem: 'Dinheiro', categoria: 'dinheiro', descricao: 'Precisamos de 300 mil até o final do ano!' },
+      { nomeItem: 'Ração', categoria: 'pets', descricao: 'Precisamos de 30 kg de ração' },
+      { nomeItem: 'Roupa', categoria: 'Roupa', descricao: 'Precisamos de agasalho' },
+      { nomeItem: 'Lata de tinta', categoria: 'outros', descricao: 'Precisamos de lata de tinta' },
+    ];
   }
 }
